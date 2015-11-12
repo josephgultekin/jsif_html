8a61f53ad7a00aa088bccea349b36fd0c91735c0
Joseph Gultekin <joseph.gueltekin@gmail.com>
2015-11-13 04:20:29 +1100

    Home page template refinements

--- js/jsif.js
+++ js/jsif.js
@@ -1,25 +1,35 @@
 /**
  * JSIF custom Javascript
  */
+// A $( document ).ready() block.
+$( document ).ready(function() {
+	$("#tour-detail-gallery").carouFredSel({
+		circular    : true,
+		infinite    : false,
+		responsive	: true,
+		width		: '100%',		// automatically calculated
+		height		: '50%',		// automatically calculated
+		prev : "#foo1_prev",
+		next : "#foo1_next",
+		scroll		: {
+			fx			: "crossfade"
+		},
+		auto		: {
+			play			: false,
+			timeoutDuration	: null		//	5 * auto.duration
+		},
+		items		: {
+			visible		: 1,
+			width		: null,
+			height		: '50%'
+		}
+	});
+	
+	$(".tours-grid-inner .thumb .title").mouseover(function(){
+		
+	});
 
-$("#tour-detail-gallery").carouFredSel({
-	circular    : true,
-	infinite    : false,
-	responsive	: true,
-	width		: '100%',		// automatically calculated
-	height		: '50%',		// automatically calculated
-	prev : "#foo1_prev",
-	next : "#foo1_next",
-	scroll		: {
-		fx			: "crossfade"
-	},
-	auto		: {
-		play			: false,
-		timeoutDuration	: null		//	5 * auto.duration
-	},
-	items		: {
-		visible		: 1,
-		width		: null,
-		height		: '50%'
-	}
+	$(".tours-grid-inner .thumb .title").mouseout(function(){
+
+	});
 });
